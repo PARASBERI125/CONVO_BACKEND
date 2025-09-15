6569e48f6fd84c12dccbd37f5a875e7571e6da67
paras beri <130133581+PARASBERI125@users.noreply.github.com>
2025-09-15 10:10:40 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDb = async (url) => {
   try {
-    await mongoose.connect(url);
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected successfully to database");
   } catch (error) {
     console.log("Error connecting the database", error);

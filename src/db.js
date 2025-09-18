c55d0a077c6f9a73bb987b69a3bf71eeee52945f
paras beri <130133581+PARASBERI125@users.noreply.github.com>
2025-09-18 11:43:35 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -7,6 +7,16 @@ const connectDb = async (url) => {
       useUnifiedTopology: true,
     });
     console.log("Connected successfully to database");
+
+ setInterval(async ()=>{
+    try{
+     await mongoose.connection.db.admin().command({ping:1});
+      console.log("Pinged MongoDB to keep it awake");
+   }catch(err){
+     console.log("MongoDB ping failed",err);
+   }
+ } , 14*60*1000 );
+    
   } catch (error) {
     console.log("Error connecting the database", error);
   }
